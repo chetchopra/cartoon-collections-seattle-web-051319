4fc9820726d2bec5fa917cb2695fe1b8a3892c9b
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 19:29:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def summon_captain_planet(arr)
 end
 
 def long_planeteer_calls(call)
-  call.length > 4 ? true : false
+  call.length > 4 ? TRUE : false
 end
 
 def find_the_cheese# code an argument here

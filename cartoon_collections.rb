9cbeff09f709e47eed8fae8cbb99dc0e4d593f18
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 19:36:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,9 @@ def long_planeteer_calls(arr)
   return true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+long_planeteer_calls()
+
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end

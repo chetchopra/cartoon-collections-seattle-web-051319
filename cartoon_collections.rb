91ee014e8512a5bb540c6acc6d478f6d962bdbb0
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 19:21:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,14 @@ def summon_captain_planet(arr)
   return arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+puts summon_captain_planet(["fgjds", "fjd"])
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
+
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end

43d9775f56b3db2a1c6277a065b859596ec7bc58
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 19:19:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,14 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
+  count = 0
   arr.each do |n|
     change = n
     n.upcase
-  
+    change += "!"
+    arr[count] = change
+    count += 1
+  end
 end
 
 def long_planeteer_calls# code an argument here
